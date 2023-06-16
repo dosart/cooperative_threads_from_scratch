6011d33fd84c24c391e6e44a93e533979de4d933
dosart <Dysprosium92@ya.ru>
2023-06-16 19:01:46 +0300

    refactor: change the type of my_turn variable in the ticket_mutex_lock function

--- src/ticket_mutex.c
+++ src/ticket_mutex.c
@@ -1,16 +1,16 @@
 #include "ticket_mutex.h"
 
-void ticket_mutex_init(ticket_mutex_t* mutex) {
-    mutex->ticket = 0;
-    mutex->turn = 0;
+void ticket_mutex_init(ticket_mutex_t *mutex) {
+  mutex->ticket = 0;
+  mutex->turn = 0;
 }
 
-void ticket_mutex_lock(ticket_mutex_t* mutex) {
-    atomic_int my_turn = atomic_fetch_and(&mutex->ticket, 1);
-    while (atomic_load(&mutex->turn) != my_turn)
+void ticket_mutex_lock(ticket_mutex_t *mutex) {
+  unsigned int my_turn = atomic_fetch_and(&mutex->ticket, 1);
+  while (atomic_load(&mutex->turn) != my_turn)
     ;
 }
 
-void ticket_mutex_unlock(ticket_mutex_t* mutex) {
-    atomic_fetch_and(&mutex->turn, 1);
+void ticket_mutex_unlock(ticket_mutex_t *mutex) {
+  atomic_fetch_and(&mutex->turn, 1);
 }

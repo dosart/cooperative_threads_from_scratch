acfcd6bc89f7cd9870b829ac3b676b43986b63b4
dosart <Dysprosium92@ya.ru>
2023-06-21 19:39:26 +0300

    fix: name in file include/ticket_mutex.h

--- include/ticket_mutex.h
+++ include/ticket_mutex.h
@@ -3,18 +3,15 @@
 
 #include <stdatomic.h>
 
-
 typedef struct __ticket_mutex_t {
-    atomic_uint ticket;
-    atomic_uint turn;
+  atomic_uint ticket;
+  atomic_uint turn;
 } ticket_mutex_t;
 
-void ticket_mutex_init(ticket_mutex_t* mutex);
+void ticket_mutex_init(ticket_mutex_t *mutex);
 
-void ticket_mutex_lock(ticket_mutex_t* mutex);
+void ticket_mutex_lock(ticket_mutex_t *mutex);
 
-void ticket_mutex_unlock(ticket_mutex_t* mutex);
-
-
+void ticket_mutex_unlock(ticket_mutex_t *mutex);
 
 #endif //_TICKET_MUTEX_H
